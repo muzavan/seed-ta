864eb00ec42a765d494e502327bf6ebf49ac4281
Muhammad Reza Irvanda <muzavan@gmail.com>
2016-05-17 19:10:22 +0700

    CSV MONGO

---
+++ csv_mongo.js
@@ -0,0 +1,28 @@
+var csv = require("fast-csv");
+var path = "contoh_data.csv";
+var mongodb = require('mongodb');
+
+var client = mongodb.MongoClient;
+var url = "mongodb://user:27017/pendidikan";
+var documents = [];
+client.connect(url,function(err,db){
+    if(err){
+        console.log("Unable to connect to the MongoDB Server. Error : ",error);
+    }
+    else{
+        console.log('Connection established to', url);
+        
+        csv
+        .fromPath(path,{headers : true, delimiter : ';'})
+        .on("data", function(data){
+            documents.push(data);
+        })
+        .on("end", function(){
+            var collection = db.collection("sd");
+            collection.insert(documents);
+            console.log("INSERTING : "+documents.length);
+            db.close(); 
+        });
+        
+    }
+});
