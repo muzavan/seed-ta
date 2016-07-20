2d78e305bb20ed5f5cad935ec4d0e720790515c9
Muhammad Reza Irvanda <muzavan@gmail.com>
2016-07-20 16:14:12 +0700

    Edit Seed Redis

--- csv_redis.js
+++ csv_redis.js
@@ -10,10 +10,30 @@ client.on("connect", function(){
 csv
  .fromPath(path,{headers : true, delimiter : ';'})
  .on("data", function(data){
-     client.hmset("id-"+data["No"],data);
+     if(data["No"] < 15){
+         var d = data;
+         var x = {
+           "Guru/Teacher" : d["Guru/Teacher"],
+           "Kelas/Class" : d["Kelas/Class"],
+           "Murid/Pupils" : d["Murid/Pupils"],
+           "Sekolah/School" : d["Sekolah/School"],
+             
+         };//d["No"]+","+d["Guru/Teacher"]+",'"+d["Kecamatan "]+"',"+d["Kelas/Class"]+","+d["Murid/Pupils"]+","+d["Sekolah/School"]+");";
+         client.hmset("id-"+data["No"],x);
+     }
+     else{
+		var d = data;
+        var x = {};
+		
+		for(var k in d){
+			if(k != "Guru/Teacher"){
+				x[k] = d[k];
+			}
+		}
+        client.hmset("id-"+data["No"],x);   
+     }
  })
  .on("end", function(){
      console.log("Done");
+	 client.quit();
  });
- 
- client.quit();
